f53baaf31d42d34be6cb8aca53a919d912d32b0c
Saurabh Patel <saurabh.finch@gmail.com>
2020-07-30 15:19:48 +0530

    complete step 1

--- web/config.js
+++ web/config.js
@@ -1,7 +1,19 @@
 'use strict'
+const joi = require('joi');
 
+const schema = joi.object({
+  port: joi.number().required().min(3001).max(65555)
+});
+
+const envVars = process.env;
 const config = {
-  port: 0
+  port: envVars.PORT
 }
+
+const { error, value } = schema.validate(config);
+if (error) {
+  throw new Error(error);
+}
+
 
 module.exports = config
--- web/server.js
+++ web/server.js
@@ -2,11 +2,19 @@
 
 const Koa = require('koa')
 const logger = require('winston')
+const Router = require('koa-router');
 
 const app = new Koa()
+const router = new Router();
+
+router.get('/hello', (ctx, next) => {
+  ctx.body = 'Hello Node.js!';
+});
 
 app.on('error', (err) => {
   logger.error('Server error', { error: err.message })
 })
+
+app.use(router.routes()).use(router.allowedMethods());
 
 module.exports = app
